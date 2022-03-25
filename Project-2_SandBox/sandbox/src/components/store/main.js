155295b2776c9fa994c8cbeb3b6e596e8a4f1907
blueberry <kuangyongchang@gmail.com>
2022-03-25 11:24:34 +0800

    local storage

--- Project-2_SandBox/sandbox/src/components/store/main.js
+++ Project-2_SandBox/sandbox/src/components/store/main.js
@@ -4,8 +4,24 @@ import userReducer from "./user";
 import chatReducer from "./chat";
 import loaderReducer from "./loader";
 
+const localStorageMiddleware = ({ getState }) => {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem("state", JSON.stringify(getState()));
+    return result;
+  };
+};
+
+const reStore = () => {
+  if (localStorage.getItem("state") !== null) {
+    return JSON.parse(localStorage.getItem("state")); // re-hydrate the store
+  }
+};
+
 const store = configureStore({
   reducer: { user: userReducer, auth: authReducer, loader: loaderReducer, chat: chatReducer },
+  preloadedState: reStore(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export default store;
